30b1717bd589bbb33a9e47f3a186ad0412e12503
Max Mitchell <maxwell.w.mitchell@gmail.com>
2014-03-01 09:17:24 -0600

    began problem 3

---
+++ code/problem_3.R
@@ -0,0 +1,8 @@
+# problem: 3 - largest prime factor
+# url: http://projecteuler.net/problem=3
+
+# find the largest prime factor of 600851475143
+#   method 1:
+#   - list all prime numbers below sqrt(600851475143) (primes)
+#   - search for factors from primes (factors)
+#   - 
