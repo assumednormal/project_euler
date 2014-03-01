b66fe6d16c26f8b001fec8e47417be4bfcdf9beb
Max Mitchell <maxwell.w.mitchell@gmail.com>
2014-03-01 16:26:00 -0600

    completed problem 4

--- code/problem_4.R
+++ code/problem_4.R
@@ -4,19 +4,33 @@
 # find the largest palindrome made from the product of two 3-digit numbers
 #   method 1:
 #   - find all products of two 3-digit numbers (products)
+#   - remove all products divisible by 10 (products)
+#   - 
 
 
 # method 1 ----------------------------------------------------------------
 
 # products of all 3-digit numbers
-products <- vector(mode = "character", length = 900 * 901 / 2)
+products <- vector(mode = "integer", length = 900 * 901 / 2)
 idx <- 1
 for(i in 100:999) {
   for(j in i:999) {
-    products[idx] <- paste0(i * j)
+    products[idx] <- i * j
     idx <- idx + 1
   }
 }
 products <- sort(x = unique(x = products))
 
-# 
+# remove all numbers divisible by 10
+products <- products[products %% 10 != 0]
+
+# find all palindromes palindrome
+palindromes <- sapply(X = products, FUN = function(p) {
+  palindrome <- 0
+  char_p <- strsplit(x = paste0(p), split = NULL)[[1]]
+  if(identical(x = char_p, y = rev(x = char_p))) palindrome <- 1
+  palindrome
+})
+
+# find largest number that is also a palindrome
+answer1 <- max(products[palindromes == 1])
