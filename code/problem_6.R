226bba9474396d708b8f022470e6ae1b387d936b
Max Mitchell <maxwell.w.mitchell@gmail.com>
2014-03-01 18:02:50 -0600

    completed problem 6

--- code/problem_6.R
+++ code/problem_6.R
@@ -1,4 +1,21 @@
 # problem: 6 - sum square difference
 # url: http://projecteuler.net/problem=6
 
-# 
+# find the difference between the sum of the squares and the square of the sum of
+# the first 100 natural numbers: 1, 2, 3, ..., 100
+#   method 1:
+#   - calculate the sum of the squares (sum_sq)
+#   - calculate the square of the sum (sq_sum)
+#   - take the difference of sum_sq and sq_sum (answer1)
+
+
+# method 1 ----------------------------------------------------------------
+
+# calculate the sum of the squares
+sum_sq <- sum((1:100)^2)
+
+# calculate the square of the sum
+sq_sum <- sum(1:100)^2
+
+# take the difference of sum_sq and sq_sum
+answer1 <- abs(sum_sq - sq_sum)
