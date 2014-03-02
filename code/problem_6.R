c7903c0269eb9bedc87028875580b94c3407e684
Max Mitchell <maxwell.w.mitchell@gmail.com>
2014-03-02 10:40:49 -0600

    updated problem 6 to include calculations using the closed forms for sums

--- code/problem_6.R
+++ code/problem_6.R
@@ -7,7 +7,11 @@
 #   - calculate the sum of the squares (sum_sq)
 #   - calculate the square of the sum (sq_sum)
 #   - take the difference of sum_sq and sq_sum (answer1)
-
+#
+#   method 2:
+#   - calculate the sum of the squares using the closed form (sum_sq_closed)
+#   - calculate the square of the sum using the closed form (sq_sum_closed)
+#   - take the difference of sum_sq_closed and sq_sum_closed (answer2)
 
 # method 1 ----------------------------------------------------------------
 
@@ -19,3 +23,15 @@ sq_sum <- sum(1:100)^2
 
 # take the difference of sum_sq and sq_sum
 answer1 <- abs(sum_sq - sq_sum)
+
+
+# method 2 ----------------------------------------------------------------
+
+# calculate the sume of the squares using the closed form
+sum_sq_closed <- 100 * 101 * 201 / 6
+
+# calculate the square of the sum using the closed form
+sq_sum_closed <- (100 * 101 / 2)^2
+
+# take the difference of sum_sq_closed and sq_sum_closed
+answer2 <- abs(sum_sq_closed - sq_sum_closed)
